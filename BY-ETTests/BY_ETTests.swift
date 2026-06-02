5dbe333a249c882513d53d4738c01853341c8417
SSSiye <tldp6036@naver.com>
2026-06-02 16:28:52 +0900

    Initial Commit

---
+++ BY-ETTests/BY_ETTests.swift
@@ -0,0 +1,17 @@
+//
+//  BY_ETTests.swift
+//  BY-ETTests
+//
+//  Created by siye on 6/2/26.
+//
+
+import Testing
+@testable import BY_ET
+
+struct BY_ETTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BY-ETUITests/BY_ETUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BY_ETUITestsLaunchTests.swift
+//  BY-ETUITests
+//
+//  Created by siye on 6/2/26.
+//
+
+import XCTest
+
+final class BY_ETUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
