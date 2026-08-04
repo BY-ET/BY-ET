278f07047ec2ca5cdea89ef3db0cb04dd1328897
SSSiye <tldp6036@naver.com>
2026-08-04 12:12:08 +0900

    [Design] #14 - fonts 정리

---
+++ BY-ET/Extensions/Fonts.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+extension Font {
+    static let Display = Font.custom("Pretendard-Bold", size: 32)
+    static let Headline = Font.custom("Pretendard-Bold", size: 24)
+    static let Title = Font.custom("Pretendard-Bold", size: 16)
+    static let Navigation = Font.custom("Pretendard-Medium", size: 20)
+    
+    
+    static let Bodyoption = Font.custom("Pretendard-Bold", size: 20)
+    static let Bodymedium = Font.custom("Pretendard-Medium", size: 16)
+    static let Bodybtn = Font.custom("Pretendard-Medium", size: 16)
+    static let Bodyregular = Font.custom("Pretendard-Regular", size: 16)
+    
+    
+    static let footnotemedium = Font.custom("Pretendard-Medium", size: 14)
+    static let footnoteregular = Font.custom("Pretendard-Regular", size: 14)
+    static let caption = Font.custom("Pretendard-Regular", size: 12)
+    
+    static let Btnlarge = Font.custom("Pretendard-Bold", size: 20)
+    static let Btnmedium = Font.custom("Pretendard-Bold", size: 16)
+}
